36d8c7c859e5ec47e049f7d07265b9736164f730
Steven <steven200796@gmail.com>
2015-08-30 19:51:18 -0400

    Mailing Server Working

--- contact/contact-form-handler.php
+++ contact/contact-form-handler.php
@@ -1,7 +1,17 @@
 
 <?php
 
-if($_POST["submit"]) {
+function debug( $data ) {
+
+    if ( is_array( $data ) )
+        $output = "<script>console.log( 'Debug Objects: " . implode( ',', $data) . "' );</script>";
+    else
+        $output = "<script>console.log( 'Debug Objects: " . $data . "' );</script>";
+
+    echo $output;
+}
+
+if(isset($_POST["cd-email"])) {
     $recipient="steven@stevenshi.me";
     $subject="You have received a new message from your site.";
     $sender=$_POST["cd-name"];
@@ -9,19 +19,23 @@ if($_POST["submit"]) {
     $senderCompany=$_POST["cd-company"];
     $message=$_POST["cd-textarea"];
     $aReasons=$_POST['contactReasons'];
+    debug( "pass1");
     if(!empty($aReasons)) {
         $reasons="";
         $N = count($aReasons);
+	dbug( "pass2" );
         for($i=0; $i < $N; $i++)
         {
-            $reasons .= "$aReasons[$i] ";
+            $reasons .= "{$aReasons}[{$i}] ";
         }
     }
 
     $mailBody="Name: $sender\nEmail: $senderEmail\n Reasons: $reasons\n\n$message";
 
     mail($recipient, $subject, $mailBody, "From: $sender <$senderEmail>");
-
+    
+    debug( "pass3" );
     header('Location: contact-form-thank-you.html');}
-
+    die();
+    debug( "pass4" );
 ?>
