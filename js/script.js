1e57fd511a596e1a92bde5d05e87b49dfd701dd7
steven200796 <steven200796@gmail.com>
2015-09-04 01:38:23 -0400

    Finished Basic Home Page, Need to Fix Resume Page

---
+++ js/script.js
@@ -0,0 +1,31 @@
+$("#images > div:gt(0)").hide();
+$("#verbs > span").hide();
+
+cycleWords();
+
+/* Init values 6000 and 1000 */
+var cycleTime = 15000;
+var fadeTime = 1000;
+
+function cycleWords() {
+    $('#verbs > span:first')
+        .delay(500)
+        .fadeIn(fadeTime)
+        .delay(3500)
+        .fadeOut(fadeTime)
+        .next()
+        .end()
+        .appendTo('#verbs');
+}
+
+function cycleBackgrounds() {
+    $('#images > div:first')
+        .fadeOut(fadeTime)
+        .next()
+        .fadeIn(fadeTime)
+        .end()
+        .appendTo('#images');
+}
+
+setInterval(cycleBackgrounds, cycleTime);
+setInterval(cycleWords, cycleTime);
