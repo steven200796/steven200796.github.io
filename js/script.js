b8078115ca6cd34e4ba606df57fab3ee9b312dac
steven200796 <steven200796@gmail.com>
2015-09-04 21:21:36 -0400

    Fine tuning some css, added colored navigation

--- js/script.js
+++ js/script.js
@@ -1,31 +1,30 @@
-$("#images > div:gt(0)").hide();
-$("#verbs > span").hide();
+            $("#images > div:gt(0)").hide();
+            $("#verbs > span").hide();
+            cycleWords();
 
-cycleWords();
+            /* Init values 6000 and 1000 */
+            var cycleTime = 6000;
+            var fadeTime = 1000;
 
-/* Init values 6000 and 1000 */
-var cycleTime = 15000;
-var fadeTime = 1000;
+            function cycleWords() {
+                $('#verbs > span:first')
+                    .delay(500)
+                    .fadeIn(fadeTime)
+                    .delay(3500)
+                    .fadeOut(fadeTime)
+                    .next()
+                    .end()
+                    .appendTo('#verbs');
+            }
 
-function cycleWords() {
-    $('#verbs > span:first')
-        .delay(500)
-        .fadeIn(fadeTime)
-        .delay(3500)
-        .fadeOut(fadeTime)
-        .next()
-        .end()
-        .appendTo('#verbs');
-}
+            function cycleBackgrounds() {
+                $('#images > div:first')
+                    .fadeOut(fadeTime)
+                    .next()
+                    .fadeIn(fadeTime)
+                    .end()
+                    .appendTo('#images');
+            }
 
-function cycleBackgrounds() {
-    $('#images > div:first')
-        .fadeOut(fadeTime)
-        .next()
-        .fadeIn(fadeTime)
-        .end()
-        .appendTo('#images');
-}
-
-setInterval(cycleBackgrounds, cycleTime);
-setInterval(cycleWords, cycleTime);
+            setInterval(cycleBackgrounds, cycleTime);
+            setInterval(cycleWords, cycleTime);
