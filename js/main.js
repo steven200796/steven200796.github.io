c533298dde83b7afd0f537a7ac79e2ad1f72e83c
Di Yang Shi <steven200796@gmail.com>
2015-09-08 15:34:16 -0400

    Putting main.js in head

--- js/main.js
+++ js/main.js
@@ -1,5 +1,35 @@
-jQuery(document).ready(function ($) {
+$(document).ready(function ($) {
 
+    $("#images > div:gt(0)").hide();
+    $("#verbs > span").hide();
+    cycleWords();
+
+    /* Init values 6000 and 1000 */
+    var cycleTime = 6000;
+    var fadeTime = 1000;
+
+    function cycleWords() {
+        $('#verbs > span:first')
+            .delay(1000)
+            .fadeIn(fadeTime)
+            .delay(3500)
+            .fadeOut(fadeTime)
+            .next()
+            .end()
+            .appendTo('#verbs');
+    }
+
+    function cycleBackgrounds() {
+        $('#images > div:first')
+            .fadeOut(fadeTime)
+            .next()
+            .fadeIn(fadeTime)
+            .end()
+            .appendTo('#images');
+    }
+
+    setInterval(cycleBackgrounds, cycleTime);
+    setInterval(cycleWords, cycleTime);
     $("a.trigger").click(function (event) {
         event.preventDefault();
         linkLocation = $(this).attr("href");;
